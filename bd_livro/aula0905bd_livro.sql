3d6b0f7ee5d9d8b227da8d8390f94af91d174537
Gabriel Mousinho Daia <126831870+GabrielMousinho@users.noreply.github.com>
2025-09-12 21:26:13 -0300

    Delete bd_livro directory
    Mudança de nome.

--- bd_livro/aula0905bd_livro.sql
+++
@@ -1,9 +0,0 @@
-create database bd_livro_01;
-
-use bd_livro_01;
-
-create table tb_genero (cd_genero int not null primary key, genero char(30));
-
-create table tb_editora (cd_editora int not null primary key, editora char(30));
-
-create table tb_livro (isbn int not null primary key, titulo char(50), cd_editora int, cd_genero int, preco money);
