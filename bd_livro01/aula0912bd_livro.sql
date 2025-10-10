ef1255da5080c1ab689dda9d11d437c898db52f7
Gabriel Mousinho Daia <126831870+GabrielMousinho@users.noreply.github.com>
2025-10-10 20:03:47 -0300

    Update aula0912bd_livro.sql
    Atualização de comandos.

--- bd_livro01/aula0912bd_livro.sql
+++ bd_livro01/aula0912bd_livro.sql
@@ -1,8 +1,25 @@
 USE bd_livro_01;
 
-INSERT INTO tb_editora
+INSERT INTO tb_editora VALUES
+	(1, 'novatec'),
+	(2, 'amazon'),
+	(3, 'coopmed'),
+	(4, 'livraria florence'),
+	(5, 'blucher'),
+	(6, 'Mundial'),
+	(7, 'saraiva'),
+	(8, 'Editora Forum'),
+	(9, 'Dickens');
 
-INSERT INTO tb_genero
+INSERT INTO tb_genero VALUES
+	(1, 'Computacao'),
+	(2, 'Medicina'),
+	(3, 'Engenharia'),
+	(4, 'Juridico'),
+	(5, 'Arquitetura'),
+	(6, 'Biologia'),
+	(7, 'Mecatronica'),
+	(8, 'Publicidade');
 
 INSERT INTO tb_livro VALUES 
 	(1, 'banco de dados', 1, 1, 300.00),
@@ -16,4 +33,5 @@ INSERT INTO tb_livro VALUES
 	(9, 'Cores', 7, 5, 200.00),
 	(10, 'Paisagismo', 8, 5, 250.00),
 	(11, 'Virus', 9, 6, 300.00),
+
 	(12, 'Bacteria', 9, 6, 300.00);
